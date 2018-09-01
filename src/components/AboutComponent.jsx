c24e2f01bf030eacecfe7cbb6463001e060a66b9
nadun94 <nadunsirimevan@gmail.com>
2018-09-01 10:22:18 +0530

    initial commit before adding to github

---
+++ src/components/AboutComponent.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import '../assets/css/main.css'
+export default class Header extends Component {
+
+// constructor(){
+// 	super();
+
+// 	state=[
+
+// 	]
+// }
+	render () {
+		return (
+			<div>
+				<div className="navbar">
+				Hey studentsnp
+				</div>
+			</div>
+		);
+	}
+}
